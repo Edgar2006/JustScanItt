312364b4fa5e5be271fe5e222cdcbef1e54e8de0
Edgar Bezhanyan <102021927+Edgar2006@users.noreply.github.com>
2025-07-04 15:53:16 +0400

    Add product bio parametrs and default countrcre geter seter

---
+++ app/src/main/java/com/example/justscanitt/Class/ProductBio.java
@@ -0,0 +1,63 @@
+package com.example.justscanitt.Class;
+
+public class ProductBio {
+
+    private String companyEmail, companyName, productName, imageRef, bio, barCode;
+
+    public ProductBio(String companyEmail, String companyName, String productName, String imageRef, String bio, String barCode) {
+        this.companyEmail = companyEmail;
+        this.companyName = companyName;
+        this.productName = productName;
+        this.imageRef = imageRef;
+        this.bio = bio;
+        this.barCode = barCode;
+    }
+
+    public String getCompanyEmail() {
+        return companyEmail;
+    }
+
+    public void setCompanyEmail(String companyEmail) {
+        this.companyEmail = companyEmail;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getImageRef() {
+        return imageRef;
+    }
+
+    public void setImageRef(String imageRef) {
+        this.imageRef = imageRef;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getBarCode() {
+        return barCode;
+    }
+
+    public void setBarCode(String barCode) {
+        this.barCode = barCode;
+    }
+}
