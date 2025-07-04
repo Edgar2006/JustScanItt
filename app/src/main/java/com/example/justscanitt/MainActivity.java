95a834af8859c7935382a5222c0924048c8b24a0
SusannaAvdalyan <susanna.avdalyan.g@tumo.org>
2025-07-04 13:57:26 +0400

    Login setup

--- app/src/main/java/com/example/justscanitt/MainActivity.java
+++ app/src/main/java/com/example/justscanitt/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.justscanitt;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +13,29 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 public class MainActivity extends AppCompatActivity {
 
+    private EditText emailInput, nameInput;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        emailInput = findViewById(R.id.emailInput);
+        nameInput = findViewById(R.id.nameInput);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void onClickSignIn(View view) {
     }
 }
