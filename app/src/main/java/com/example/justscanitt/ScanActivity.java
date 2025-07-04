6935a5a8e4ab328f45557a6a9fd2c1ce8a837fe9
Edgar Bezhanyan <102021927+Edgar2006@users.noreply.github.com>
2025-07-04 15:19:12 +0400

    Add test barCode scanner

--- app/src/main/java/com/example/justscanitt/ScanActivity.java
+++ app/src/main/java/com/example/justscanitt/ScanActivity.java
@@ -1,24 +1,61 @@
 package com.example.justscanitt;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class ScanActivity extends AppCompatActivity {
-
+    private String barCode = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_scan);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
+    public void onCLickScanNow(View view){
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setCaptureActivity(CaptureAct.class);
+        integrator.setOrientationLocked(false);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+        integrator.setPrompt("Scanning Code");
+        integrator.initiateScan();
+    }
+
+    // Called when scanner returns result
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if(result != null){
+            if(result.getContents() != null){
+                try {
+                    // Get scanned barcode and update input field
+                    barCode = result.getContents();
+                    push_activity(); // Open read screen
+                }catch (Exception ignored){
+                }
+            }
+        }else{
+            // Default behavior for other activities
+            super.onActivityResult(requestCode,resultCode,data);
+        }
+
+    }
+
+    // Open Read activity to display product info by barcode
+    public void push_activity(){
+        Toast.makeText(this, "barCode", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(ScanActivity.this, ReadActivity.class);
+        intent.putExtra("barCode", barCode);
+        startActivity(intent);
+    }
+
+
+
 }
